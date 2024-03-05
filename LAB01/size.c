8f598a2a201a810519732f1c07d3e7c3c1a70d3c
Dawid Olko <dawid_olko@outlook.com>
2024-03-05 23:36:00 +0100

    Update size.c

--- LAB01/size.c
+++ LAB01/size.c
@@ -19,16 +19,6 @@ int main(){
     
     printf("\n");
     
-    printf("Rozmiar unsigned char: %d\n", sizeof(unsigned char));
-    printf("Rozmiar unsigned short: %d\n", sizeof(unsigned short));
-    printf("Rozmiar unsigned int: %d\n", sizeof(unsigned int));
-    printf("Rozmiar unsigned long: %d\n", sizeof(unsigned long));
-    printf("Rozmiar unsigned long int: %d\n", sizeof(unsigned long int));
-    printf("Rozmiar unsigned long long: %d\n", sizeof(unsigned long long));
-    
-    printf("\n");
-    
-    
     
     return 0;
 }
