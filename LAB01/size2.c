83f6613e73d30c35fd1f94e0989a027587d480c2
Dawid Olko <dawid_olko@outlook.com>
2024-03-05 23:36:26 +0100

    Create size2.c

---
+++ LAB01/size2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    printf("size2.c\n\n");
+    
+    printf("\n");
+    
+    printf("Rozmiar unsigned char: %d\n", sizeof(unsigned char));
+    printf("Rozmiar unsigned short: %d\n", sizeof(unsigned short));
+    printf("Rozmiar unsigned int: %d\n", sizeof(unsigned int));
+    printf("Rozmiar unsigned long: %d\n", sizeof(unsigned long));
+    printf("Rozmiar unsigned long int: %d\n", sizeof(unsigned long int));
+    printf("Rozmiar unsigned long long: %d\n", sizeof(unsigned long long));
+    
+    printf("\n");
+    
+    
+    
+    return 0;
+}
